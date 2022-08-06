543aca75980968c49b34f4e90d06e5feeef9929a
hakrimryu <hakrimryu@gmail.com>
2022-08-06 13:29:28 +0900

    1313

--- PapagoBot.py
+++ PapagoBot.py
@@ -194,13 +194,9 @@ async def on_message(message):  # on_message() event : when the bot has recieved
                         value=f":game_die: {str(a)}가 나왔습니다. (1-999)", inline=False)
         await message.channel.send(embed=embed)
 
-    if message.content.startswith("~test1"):
-        async def roll(ctx, a1):
-            await ctx.send(f'투표 시작 {a1}')
-
-    if message.content.startswith("~test2"):
-        async def rolls(ctx, a1):
-            await message.channel.send(f'투표 시작 {a1}')
+    if message.content.startswith("~test"):
+        trsText = message.content.split(" ")
+        await message.channel.send(trsText)
 
 ##@client.command(name='test')
 ##async def roll(ctx, a1):

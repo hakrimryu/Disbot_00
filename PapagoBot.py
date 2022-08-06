2ee53c18bb389d6bbcbc5f3775a25cc4ca2ae0b9
hakrimryu <hakrimryu@gmail.com>
2022-08-06 14:06:59 +0900

    131313

--- PapagoBot.py
+++ PapagoBot.py
@@ -196,8 +196,9 @@ async def on_message(message):  # on_message() event : when the bot has recieved
                         value=f":game_die: {str(a)}가 나왔습니다. (1-999)", inline=False)
         await message.channel.send(embed=embed)
 
-@client.command()
-async def test(ctx, duration="0:0:0", question="질문", *answers):
+@client.command(name='투표')
+@commands.is_owner
+async def vote(ctx, duration="0:0:0", question="질문", *answers):
     # Poll attributes
     duration = list(map(int, duration.split(":")))
     multi = False
